7130ae0638a430b9aad01d507887e5597a9d3823
PrinceSoni83 <prince.git.bucket@gmail.com>
2019-12-04 14:28:15 +0400

    updated the code

--- Misc/anagram.py
+++ Misc/anagram.py
@@ -20,12 +20,10 @@ def anagram1(s1,s2):
 # 3. if strings are equal they are anagarm                              #
 #########################################################################
 
-#def anagram2(s1,s2):
-#     s1 = s1.replace(' ','').lower()
-#     s2 = s2.replace(' ','').lower()
-
-
-#     return sorted(s1) == sorted(s2)
-
+def anagram2(s1,s2):
+    s1 = s1.replace(' ','').lower()
+    s2 = s2.replace(' ','').lower()
+    return sorted(s1) == sorted(s2)
 
 print(anagram1('dog','god '))
+print(anagram2('dog','god '))
